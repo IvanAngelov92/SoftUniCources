a864f97fe20aef865316f92643670fe03cb3431c
IvanAngelov92 <iv921992@abv.bg>
2024-02-28 15:06:57 +0200

    Unit Testing - Methods

---
+++ Programming Fundamentals and Unit Testing-January-2024/Unit Testing - Methods/TestApp.UnitTests/FactorialTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.UnitTests;
+
+public class FactorialTests
+{
+    [Test]
+    public void Test_CalculateFactorial_InputZero_ReturnsOne()
+    {
+        // Arrange
+        int input = 0;
+
+        // Act
+        int result = Factorial.CalculateFactorial(input);
+
+        // Assert
+        Assert.AreEqual(1, result);
+
+    }
+
+    [Test]
+    public void Test_CalculateFactorial_InputPositiveNumber_ReturnsCorrectFactorial()
+    {
+        // Arrange
+        int input = 3;
+
+        // Act
+        int result = Factorial.CalculateFactorial(input);
+
+        // Assert
+        Assert.AreEqual(6, result);
+    }
+}
---
+++ Programming Fundamentals and Unit Testing-January-2024/Unit Testing - Methods/TestApp.UnitTests/StringReverseTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class StringReverseTests
+{
+    [Test]
+    public void Test_Reverse_WhenGivenEmptyString_ReturnsEmptyString()
+    {
+        // Arrange
+        string emptyString = string.Empty;
+
+        // Act
+        string empty = StringReverse.Reverse(emptyString);
+
+        // Assert
+        Assert.AreEqual("", empty);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenSingleCharacterString_ReturnsSameCharacter()
+    {
+        // Arrange
+        string emptyString = "A";
+
+        // Act
+        string result = StringReverse.Reverse(emptyString);
+
+        // Assert
+        Assert.AreEqual("A", result);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenNormalString_ReturnsReversedString()
+    {
+        // Arrange
+        string emptyString = "ABC";
+
+        // Act
+        string result = StringReverse.Reverse(emptyString);
+
+        // Assert
+        Assert.AreEqual("CBA", result);
+    }
+}
