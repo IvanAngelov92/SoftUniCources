2de1d832db81a1b49a47bdd7aab32624399e6678
IvanAngelov92 <iv921992@abv.bg>
2024-03-16 20:22:58 +0200

    Dictionaries, Lambda and LINQ - Exercise

---
+++ Programming Advanced for QA-March-2024/Dictionaries, Lambda and LINQ - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,24 @@
+﻿string resource = Console.ReadLine();
+
+Dictionary<string, int> resources = new Dictionary<string, int>();
+
+while (resource != "stop")
+{
+    int quantity = int.Parse(Console.ReadLine());
+
+    if (resources.ContainsKey(resource))
+    {
+        resources[resource] += quantity;
+    }
+    else
+    {
+        resources.Add(resource, quantity);
+    }
+
+    resource = Console.ReadLine();
+}
+
+foreach (var pair in resources)
+{
+    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+}
---
+++ Programming Advanced for QA-March-2024/Dictionaries, Lambda and LINQ - Exercise/06. Company Users/Program.cs
@@ -0,0 +1,25 @@
+﻿Dictionary<string, HashSet<string>> companies = new Dictionary<string, HashSet<string>>();
+
+string input;
+while ((input = Console.ReadLine()) != "End")
+{
+    string[] data = input.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+    string companyName = data[0];
+    string employeeId = data[1];
+
+    if (!companies.ContainsKey(companyName))
+    {
+        companies[companyName] = new HashSet<string>();
+    }
+
+    companies[companyName].Add(employeeId);
+}
+
+foreach (var kvp in companies)
+{
+    Console.WriteLine(kvp.Key);
+    foreach (var employeeId in kvp.Value)
+    {
+        Console.WriteLine($"-- {employeeId}");
+    }
+}
