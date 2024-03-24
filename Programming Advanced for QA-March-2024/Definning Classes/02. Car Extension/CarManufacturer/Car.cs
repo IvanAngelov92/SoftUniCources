ebdd6f713b33bfb8308a3532598a26032e4439a0
IvanAngelov92 <iv921992@abv.bg>
2024-03-24 14:05:01 +0200

    Definning Classes

---
+++ Programming Advanced for QA-March-2024/Definning Classes/02. Car Extension/CarManufacturer/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuesConsumption;
+
+        public string Make { get => make; set => make = value; }
+        public string Model { get => model; set => model = value; }
+        public int Year { get => year; set => year = value; }
+        public double FuelQuantity { get => fuelQuantity; set => fuelQuantity = value; }
+        public double FuelConsumption { get => fuesConsumption; set => fuesConsumption = value; }
+
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - distance * FuelConsumption > 0)
+            {
+                FuelQuantity -= distance * FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            string output = $"Make : {Make}\n" +
+                            $"Model: {Model}\n" +
+                            $"Year: {Year}\n" +
+                            $"Fuel: {FuelQuantity:F2}";
+
+            return output;
+        }
+    }
+
+}
---
+++ Programming Advanced for QA-March-2024/Definning Classes/03. Car Constructors/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuesConsumption;
+
+        public string Make { get => make; set => make = value; }
+        public string Model { get => model; set => model = value; }
+        public int Year { get => year; set => year = value; }
+        public double FuelQuantity { get => fuelQuantity; set => fuelQuantity = value; }
+        public double FuelConsumption { get => fuesConsumption; set => fuesConsumption = value; }
+
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - distance * FuelConsumption > 0)
+            {
+                FuelQuantity -= distance * FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            string output = $"Make : {Make}\n" +
+                            $"Model: {Model}\n" +
+                            $"Year: {Year}\n" +
+                            $"Fuel: {FuelQuantity:F2}";
+
+            return output;
+        }
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+    }
+
+}
