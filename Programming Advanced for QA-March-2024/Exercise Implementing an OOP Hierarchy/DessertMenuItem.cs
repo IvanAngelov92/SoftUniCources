3fefc738293850911c6dee06e812739d796ba012
IvanAngelov92 <iv921992@abv.bg>
2024-04-08 16:01:07 +0300

    Exercise: Implementing an OOP Hierarchy

---
+++ Programming Advanced for QA-March-2024/Exercise Implementing an OOP Hierarchy/DessertMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ExerciseOopHierarchy
+{
+	public class DessertMenuItem : MenuItem
+    {
+
+        public DessertMenuItem(string name, string description, decimal price)
+            : base(name, description, price)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "Dessert: " + base.ToString();
+        }
+    }
+
+}
+
---
+++ Programming Advanced for QA-March-2024/Exercise Implementing an OOP Hierarchy/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExerciseOopHierarchy
+{
+	public class Order
+	{
+
+		private List<MenuItem> _items = new();
+
+		public IReadOnlyCollection<MenuItem> Items => this._items.AsReadOnly();
+
+		public void AddItem (MenuItem menuItem)
+		{
+			this._items.Add(menuItem);
+		}
+
+		public decimal GetTotal()
+		{
+			return this._items.Select(item => item.Price).Sum();
+		}
+
+	}
+}
+
